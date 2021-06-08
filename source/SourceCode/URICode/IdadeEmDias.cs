f7d10777d4cea15e1cfeda6299d00c8cb3dc5d1e
h1fernando <hd1.fernandosouza@gmail.com>
2021-06-08 19:56:36 -0300

    feat: calculo de dias

---
+++ source/SourceCode/URICode/IdadeEmDias.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SourceCode.URICode
+{
+    public class IdadeEmDias
+    {
+        public int Dias { get; }
+
+        public IdadeEmDias(int dias)
+        {
+            Dias = dias;
+        }
+
+        public (int anos, int mess, int dias) Calcular()
+        {
+            Validar();
+            var anos = Dias / 365;
+
+            var meses = (Dias % 365) / 30;
+
+            var dias = (Dias % 365) % 30;
+
+            return (anos, meses, dias);
+        }
+
+        private void Validar()
+        {
+            if (Dias < 0)
+                throw new ArgumentException();
+        }
+    }
+}
